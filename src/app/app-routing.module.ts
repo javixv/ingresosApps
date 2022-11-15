f418091e2fbe3a9bf7fb4d67567121c047d9189d
javixv <javier.fz@outlook.com>
2022-11-14 22:02:13 -0600

    temas y rutas greadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DetalleComponent } from './ingreso-egreso/detalle/detalle.component';
+import { EstadisticaComponent } from './ingreso-egreso/estadistica/estadistica.component';
+import { IngresoEgresoComponent } from './ingreso-egreso/ingreso-egreso.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'login', component : LoginComponent},
+  {path : 'register', component : RegisterComponent},
+  {path : '', component : DashboardComponent, children : [
+    {path : 'estadistica', component : EstadisticaComponent},
+    {path : 'detalle', component : DetalleComponent},
+    {path : 'ingreso-egresos', component : IngresoEgresoComponent},
+  ]},
+  {path : '**', redirectTo : ''}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
